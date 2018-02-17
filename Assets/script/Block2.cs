f622e38b16043e03758bb98176e75fe5f53d1541
jennifermliu <jenniferliu2018@u.northwestern.edu>
2018-02-16 23:20:03 -0600

    randomly generate new boxes of different types when jumped on a box

---
+++ Assets/script/Block2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block2 : MonoBehaviour {
+
+	// Use this for initialization
+	public int index;
+	public Boolean prev;
+	void Start ()
+	{
+		//index = -1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/script/MainCharacter.cs
+++ Assets/script/MainCharacter.cs
@@ -1,7 +1,12 @@
-﻿using System.Collections;
+﻿using Random=System.Random;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 
 public class MainCharacter : MonoBehaviour
 {
@@ -21,6 +26,11 @@ public class MainCharacter : MonoBehaviour
     private GameObject camera;
     private Vector3 cameraOffset;
 
+    //variables for generating new boxes
+    private Object cylinder;
+    private Object cube;
+    private float dist;//dist between new block and current block
+    
     void Start(){
         line = GetComponent<LineRenderer>();
         camera = GameObject.Find("Main Camera");
@@ -32,12 +42,15 @@ public class MainCharacter : MonoBehaviour
         arrowScale = 0.4f;
         thrustIncrement = 5f;
         successJump = false;
+        
+        cylinder = Resources.Load("Block2");
+        cube = Resources.Load("Block1");
+        dist = 6f;
 
     }
 
     // Update is called once per frame
     void Update () {
-
 
         //Rotates character by rotationSpeed degrees per second with arrow keys
         if (Input.GetKey ("left")) {
@@ -57,7 +70,7 @@ public class MainCharacter : MonoBehaviour
         //Calculates force of jump using direction force and power of jump
         forceMagnitude = forceDirection * thrust;
 
-
+    
         //Causes character to jump once space is released, resets canJump to prevent doubleJumps
         if (Input.GetKeyUp ("space") && canJump) {
             GetComponent<Rigidbody>().AddForce(forceMagnitude, ForceMode.Impulse);
@@ -92,10 +105,50 @@ public class MainCharacter : MonoBehaviour
         {
 
             //Should register as success only when jumping on top of block, not sides - a bit buggy right now though
-
             if (ReturnDirection(collision.gameObject, this.gameObject) == HitDirection.Top){
                 successJump = true;
                 canJump = true;
+                
+                //variabel to keep tarck of direction of new block from the old block
+                //0: up
+                //1: down
+                //2: left
+                //3: right
+                int current=-1;
+                //check if colliding block is cylinder or cube
+                Block1 c1 = collision.gameObject.GetComponent<Block1>();
+                Block2 c2 = collision.gameObject.GetComponent<Block2>();
+                if (c1 != null)
+                {
+                    current=c1.index;
+                    c1.index = -1;
+                    //mark prev to true to show that this is the previous block
+                    c1.prev = true;
+                }
+                else if (c2 != null)
+                {
+                    current=c2.index;
+                    c2.index = -1;
+                    //mark prev to true to show that this is the previous block
+                    c2.prev = true;
+                }
+                GameObject[] oldblocks = GameObject.FindGameObjectsWithTag("block");
+                foreach (GameObject oldblock in oldblocks)
+                {
+                    //delete all blocks that are not colliding block or previous block
+                    if (oldblock != collision.gameObject)
+                    {
+                        if (oldblock.GetComponent<Block1>() != null && oldblock.GetComponent<Block1>().prev==false)
+                        {
+                            Destroy(oldblock);
+                        }
+                        else if (oldblock.GetComponent<Block2>() != null && oldblock.GetComponent<Block2>().prev==false)
+                        {
+                            Destroy(oldblock);
+                        }
+                    }
+                }
+                DisplayNewBoxes(current);
             }
         }
 
@@ -125,7 +178,7 @@ public class MainCharacter : MonoBehaviour
 
                 Vector3 MyNormal = MyRayHit.normal;
                 MyNormal = MyRayHit.transform.TransformDirection(MyNormal);
-
+            
                 if (MyNormal.y > 0f){
                     hitDirection = HitDirection.Top;
                 }
@@ -134,7 +187,102 @@ public class MainCharacter : MonoBehaviour
         return hitDirection;
     }
 
+    private void DisplayNewBoxes(int current)
+    {
+        Random rnd = new Random();
+        //determine shape and size of each of the three new blocks
+        int[] indices = new int[3];
+        indices[0] = rnd.Next(0, 6);
+        indices[1] = rnd.Next(0, 6);
+        indices[2] = rnd.Next(0, 6);
+        //position array for 4 directions
+        Vector3[] positions = new Vector3[4];
+        for (int i = 0; i < positions.Length; i++)
+        {
+            positions[i]= transform.position;
+            positions[i].y = 1;
+        }
+        //up
+        positions[0].z -= dist;
+        //down
+        positions[1].z += dist;
+        //left
+        positions[2].x += dist;
+        //right
+        positions[3].x -= dist;
+        if (current == 0)//up
+        {
+            GenerateABox(indices[0],positions[0],0);
+            GenerateABox(indices[1],positions[2],2);
+            GenerateABox(indices[2],positions[3],3);
+        }
+        else if (current == 1)//down
+        {
+            GenerateABox(indices[0],positions[1],1);
+            GenerateABox(indices[1],positions[2],2);
+            GenerateABox(indices[2],positions[3],3);
+        }
+        else if (current == 2)//left
+        {
+            GenerateABox(indices[0],positions[0],0);
+            GenerateABox(indices[1],positions[1],1);
+            GenerateABox(indices[2],positions[2],2);
+        }
+        else if (current == 3)//right
+        {
+            GenerateABox(indices[0],positions[0],0);
+            GenerateABox(indices[1],positions[1],1);
+            GenerateABox(indices[2],positions[3],3);
+        }
+        //if -1, don't generate new boxes
+    }
 
-    
-    
+    //generate a new box based on type represented by i, position represented by pos, 
+    //and direction of new box relative to old box represented by dir
+    private void GenerateABox(int i, Vector3 newpos, int dir)
+    {
+        
+        if (i >= 0 && i <= 2)//cylinders
+        {
+            GameObject newblock = (GameObject) Instantiate(cylinder, newpos, Quaternion.identity);
+            if (i == 0)//small
+            {
+                newblock.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+                newblock.gameObject.transform.localScale -= new Vector3(1f, 0, 1f);
+            }
+            else if (i == 1)//medium
+            {
+                newblock.gameObject.GetComponent<Renderer>().material.color = Color.red;
+                newblock.gameObject.transform.localScale -= new Vector3(0.5f, 0, 0.5f);
+            }
+            else//large
+            {
+                newblock.gameObject.GetComponent<Renderer>().material.color = Color.green;
+            }
+            Block2 newcylinder = newblock.GetComponent<Block2>();
+            newcylinder.index = dir;
+            newcylinder.prev = false;   
+        }
+        else if (i>=3 && i<=5)//cubes
+        {
+            GameObject newblock = (GameObject) Instantiate(cube, newpos, Quaternion.identity);
+            if (i == 3)//small
+            {
+                newblock.gameObject.GetComponent<Renderer>().material.color = Color.cyan;
+                newblock.gameObject.transform.localScale -= new Vector3(1f, 0, 1f);
+            }
+            else if (i == 4)//medium
+            {
+                newblock.gameObject.GetComponent<Renderer>().material.color = Color.magenta;
+                newblock.gameObject.transform.localScale -= new Vector3(0.5f, 0, 0.5f);
+            }
+            else//large
+            {
+                newblock.gameObject.GetComponent<Renderer>().material.color = Color.black;
+            }
+            Block1 newcube = newblock.GetComponent<Block1>();
+            newcube.index = dir;
+            newcube.prev = false;
+        }
+    }
 }
